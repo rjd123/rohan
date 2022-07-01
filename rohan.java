8e076279bf1b40a9f5ef3868d02096a5154db65f
rjd123 <rohandobarkar1@gmail.com>
2022-07-01 14:26:35 +0530

    Update rohan.java

--- rohan.java
+++ rohan.java
@@ -1,5 +1,10 @@
-class rohan {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-    }
-}
+
+public class rohan
+{  
+public static void main(String args[])   
+{  
+int n1 = 225, n2 = 115, sum;  
+sum = n1 + n2;  
+System.out.println("The sum of numbers is: "+sum);  
+}  
+} 
